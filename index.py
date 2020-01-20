26fb5512633574d9f68911db026ba0e4caab2118
KingYoSun <nocking257838@gmail.com>
2020-01-20 12:07:01 +0000

    Install ruamel.yaml

--- index.py
+++ index.py
@@ -1,12 +1,29 @@
+import sys
+sys.path.append('lib/ruamel.yaml')
+sys.path.append('lib/tweepy')
+
 import json
 import datetime
+import os
+import tweepy
+from ruamel.yaml import YAML
 
+#Load Key and Token
+with open('secret.yml') as file:
+    yaml = YAML(typ='safe')
+    env = yaml.load(file)
+
+#Twitterの認証 
+CK = env['TWITTER_CK']
+CS = env['TWITTER_CS']
+AT = env['TWITTER_AT']
+AS = env['TWITTER_AS']
 
 def handler(event, context):
-    data = {
-        'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat()
-    }
-    return {'statusCode': 200,
-            'body': json.dumps(data),
-            'headers': {'Content-Type': 'application/json'}}
+    #Twitterオブジェクトの生成
+    auth = tweepy.OAuthHandler(CK, CS)
+    auth.set_access_token(AT, AS)
+    
+    api = tweepy.API(auth)
+    
+    api.update_status("Hello Tweepy!")
