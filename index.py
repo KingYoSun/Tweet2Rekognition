15597375d0b0d10fe72fb35985c4f6f85fbdc24f
KingYoSun <nocking257838@gmail.com>
2020-01-23 12:27:35 +0000

    Extract Img has Person Label

--- index.py
+++ index.py
@@ -67,10 +67,15 @@ class TweetScraper:
                         "retweet_count": result.retweet_count,
                         "created_at": result.created_at.isoformat(),
                         "url": url,
-                        "media_url": []
+                        "img": []
                     })
                     for i in range(num_media):
-                        self.tweet_data[-1]["media_url"].append(result.extended_entities["media"][i]["media_url_https"])
+                        self.tweet_data[-1]["img"].append({
+                        "id": result.extended_entities["media"][i]["id_str"],
+                        "url": result.extended_entities["media"][i]["media_url_https"],
+                        "labels": [],
+                        "bounding_box": []
+                        })
                 
         except Exception as e:
             print('Twitter Search Error' + str(e))
@@ -80,7 +85,6 @@ class TweetScraper:
 class SendRekognition:
     def __init__(self, data):
         self.data = data
-        self.person_th = PERSON_THRESHOLD
 
     def send(self, img):
         try:
@@ -93,13 +97,20 @@ class SendRekognition:
     def add_labels(self):
         #各ツイート
         for i in range(len(self.data)):
-            #各画像
-            for img_url in self.data[i]["media_url"]:
-                img_in = urllib.request.urlopen(img_url).read()
+            #各ツイート内の各画像
+            for j in range(len(self.data[i]["img"])):
+                img_in = urllib.request.urlopen(self.data[i]["img"][j]["url"]).read()
                 img_bin = io.BytesIO(img_in)
-                print("text={}, url={}".format(self.data[i]["text"], img_url))
                 result = self.send(img_in)
-                print(result)
+                label_names = [l["Name"] for l in result["Labels"]] #ラベル名（Key）のリスト
+                for label in result["Labels"]:
+                    # labelが"Name": "Person"を持ち、ConfidenceがPERSON_THRESHOLDを超え、かつBoundingBoxを持つ場合
+                    if "Person" in label["Name"] and label["Confidence"] > PERSON_THRESHOLD and len(label["Instances"]) > 0 and "BoundingBox" in [b for b in label["Instances"][0]]:
+                        self.data[i]["img"][j]["labels"].append(label_names)
+                        for b in range(len(label["Instances"])):
+                            if label["Instances"][b]["Confidence"] > PERSON_THRESHOLD:
+                                self.data[i]["img"][j]["bounding_box"].append(label["Instances"][b]["BoundingBox"])
+                        break
     
 def handler(event, context):
     scraper = TweetScraper()
